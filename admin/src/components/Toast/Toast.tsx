4eb8b51cdcb39ff9a008c28826c06a020843426d
Mohamed Dhia Bouali <dhiabouali250@gmail.com>
2026-08-27 13:15:34 +0100

    feat(toast): automatically close toast messages

--- admin/src/components/Toast/Toast.tsx
+++ admin/src/components/Toast/Toast.tsx
@@ -1,34 +1,76 @@
+import { useEffect, useState } from "react";
+
 import styles from "./Toast.module.css";
 
 type ToastProps = {
     message: string;
     type?: "success" | "error";
-    onClose?: () => void;
 };
 
-function Toast({ message, type = "success", onClose }: ToastProps) {
+function Toast({
+    message,
+    type = "success",
+}: ToastProps) {
+    const [isClosing, setIsClosing] = useState(false);
+    const [isVisible, setIsVisible] = useState(true);
+
+    useEffect(() => {
+        const closeTimer = setTimeout(() => {
+            setIsClosing(true);
+        }, 3000);
+
+        const removeTimer = setTimeout(() => {
+            setIsVisible(false);
+        }, 3250);
+
+        return () => {
+            clearTimeout(closeTimer);
+            clearTimeout(removeTimer);
+        };
+    }, []);
+
+    function handleClose() {
+        setIsClosing(true);
+
+        setTimeout(() => {
+            setIsVisible(false);
+        }, 250);
+    }
+
+    if (!isVisible) {
+        return null;
+    }
+
     return (
-        <div className={`${styles.toast} ${styles[type]}`}>
+        <div
+            className={`
+                ${styles.toast}
+                ${styles[type]}
+                ${isClosing ? styles.hide : ""}
+            `}
+        >
             <span>{message}</span>
 
-            {onClose && (
-                <button onClick={onClose} className={styles.close}>
-                    <svg
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M6 6L18 18M18 6L6 18"
-                            stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                        />
-                    </svg>
-                </button>
-            )}
+            <button
+                type="button"
+                className={styles.close}
+                onClick={handleClose}
+            >
+                <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M6 6L18 18M18 6L6 18"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                    />
+                </svg>
+            </button>
         </div>
     );
 }
